825dacce97adb532b6ed6ceb39e45399a3623d6d
Oscar <oscarcerro2018@cev.com>
2019-10-10 12:12:00 +0200

    Better Unity

---
+++ Unity/LightingExamples/LightingExamples/Assets/MultiplicadorDeEsferas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplicadorDeEsferas : MonoBehaviour
+{
+	public GameObject prefabEsfera;
+	public int numEsferas;
+	public float separacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numEsferas; i++)
+		{
+			Vector3 newPosition = transform.position;
+			newPosition.x += -(numEsferas * separacion / 2f) + (i * separacion);
+			newPosition.y += 1f;
+			Instantiate(prefabEsfera, newPosition, Quaternion.identity);
+		}
+    }
+}
